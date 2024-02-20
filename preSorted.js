51506994374244b72671a95467d370949246d34e
Ryan Hinkle <ryanhinkle@Ryans-MacBook-Air-4.local>
2024-02-20 08:01:45 -0600

    Added support for detailed SOG reads

---
+++ preSorted.js
@@ -0,0 +1,256 @@
+// Paste the contents into this variable as a multiline string
+const contentText = `LAK Viktor Arvidsson To record 3+ shots (100.00%) (33.33%)
+OTT Tim Stutzle To record 1+ shots (98.08%) (25.00%)
+VGK William Karlsson To record 1+ shots (95.35%) (27.91%)
+WPG Nikolaj Ehlers To record 1+ shots (94.34%) (20.75%)
+OTT Jakob Chychrun To record 1+ shots (94.23%) (28.85%)
+COL Miles Wood To record 1+ shots (94.12%) (31.37%)
+VGK Alex Pietrangelo To record 1+ shots (94.00%) (26.00%)
+MIN Mats Zuccarello To record 1+ shots (93.48%) (28.26%)
+FLA Samuel Bennett To record 1+ shots (93.02%) (30.23%)
+DAL Joe Pavelski To record 1+ shots (92.86%) (25.00%)
+WSH Tom Wilson To record 1+ shots (92.45%) (30.19%)
+LAK Phillip Danault To record 1+ shots (92.45%) (33.96%)
+VAN Quinn Hughes To record 1+ shots (91.38%) (29.31%)
+FLA Matthew Tkachuk To record 2+ shots (90.91%) (32.73%)
+WPG Sean Monahan To record 1+ shots (90.74%) (29.63%)
+WSH John Carlson To record 1+ shots (90.57%) (26.42%)
+WPG Neal Pionk To record 1+ shots (90.57%) (35.85%)
+COL Artturi Lehkonen To record 1+ shots (90.48%) (33.33%)
+OTT Drake Batherson To record 1+ shots (90.38%) (30.77%)
+PIT Kris Letang To record 1+ shots (90.38%) (32.69%)
+NJD Timo Meier To record 1+ shots (90.24%) (21.95%)
+FLA Brandon Montour-2 To record 1+ shots (89.74%) (23.08%)
+VAN Brock Boeser To record 1+ shots (89.66%) (20.69%)
+NYI Kyle Palmieri To record 1+ shots (88.89%) (24.07%)
+WSH Dylan Strome To record 1+ shots (88.68%) (24.53%)
+CBJ Zachary Werenski To record 2+ shots (87.50%) (42.50%)
+NJD Erik Haula To record 1+ shots (87.50%) (37.50%)
+COL Devon Toews To record 1+ shots (87.50%) (32.14%)
+FLA Evan Rodrigues To record 1+ shots (87.27%) (23.64%)
+NYR Chris Kreider To record 2+ shots (87.04%) (33.33%)
+WPG Kyle Connor To record 2+ shots (86.84%) (18.42%)
+NYI Bo Horvat To record 2+ shots (86.79%) (28.30%)
+PIT Lars Eller To record 1+ shots (86.54%) (25.00%)
+FLA Aaron Ekblad To record 1+ shots (86.49%) (27.03%)
+MIN Kirill Kaprizov To record 2+ shots (85.71%) (26.53%)
+PIT Rickard Rakell To record 1+ shots (85.00%) (22.50%)
+PIT Bryan Rust To record 2+ shots (84.62%) (30.77%)
+WSH TJ Oshie To record 1+ shots (83.33%) (30.56%)
+LAK Adrian Kempe To record 2+ shots (83.02%) (28.30%)
+LAK Trevor Moore To record 2+ shots (83.02%) (22.64%)
+LAK Pierre-Luc Dubois To record 1+ shots (83.02%) (26.42%)
+COL Cale Makar To record 2+ shots (82.69%) (23.08%)
+PIT Evgeni Malkin To record 2+ shots (82.69%) (30.77%)
+PIT Drew OConnor To record 1+ shots (82.69%) (28.85%)
+VGK Nicolas Roy To record 1+ shots (82.61%) (43.48%)
+OTT Shane Pinto To record 2+ shots (81.82%) (27.27%)
+MIN Matthew Boldy To record 2+ shots (81.25%) (29.17%)
+NJD Tyler Toffoli To record 2+ shots (81.13%) (30.19%)
+CBJ Johnny Gaudreau To record 1+ shots (80.77%) (26.92%)
+LAK Matt Roy To record 1+ shots (80.77%) (28.85%)
+NYI Mathew Barzal To record 2+ shots (80.00%) (23.64%)
+CBJ Yegor Chinakhov To record 1+ shots (80.00%) (27.50%)
+CBJ Boone Jenner To record 2+ shots (79.49%) (28.21%)
+NYI Pierre Engvall To record 1+ shots (79.17%) (27.08%)
+CBJ Kirill Marchenko To record 2+ shots (78.00%) (28.00%)
+NJD Luke Hughes To record 1+ shots (77.78%) (35.19%)
+FLA Sam Reinhart To record 2+ shots (77.19%) (24.56%)
+OTT Claude Giroux To record 2+ shots (76.92%) (28.85%)
+MIN Joel Eriksson-Ek To record 3+ shots (76.36%) (25.45%)
+PIT Sidney Crosby To record 3+ shots (75.47%) (30.19%)
+DAL Jason Robertson To record 2+ shots (75.00%) (28.57%)
+WSH Alex Ovechkin To record 3+ shots (74.00%) (32.00%)
+LAK Kevin Fiala To record 2+ shots (73.58%) (22.64%)
+OTT Joshua Norris To record 2+ shots (73.33%) (31.11%)
+PIT Erik Karlsson To record 2+ shots (73.08%) (34.62%)
+VAN Elias Pettersson To record 2+ shots (72.41%) (25.86%)
+NSH Ryan OReilly To record 2+ shots (69.09%) (27.27%)
+LAK Quinton Byfield To record 2+ shots (68.63%) (33.33%)
+DAL Roope Hintz To record 2+ shots (68.52%) (27.78%)
+NYI Brock Nelson To record 3+ shots (68.52%) (29.63%)
+COL Nathan MacKinnon To record 4+ shots (68.42%) (26.32%)
+VAN Conor Garland To record 2+ shots (68.42%) (35.09%)
+WPG Cole Perfetti To record 2+ shots (67.92%) (32.08%)
+NYR Vincent Trocheck To record 2+ shots (67.86%) (21.43%)
+NYI Noah Dobson To record 2+ shots (66.67%) (22.22%)
+FLA Gustav Forsling To record 2+ shots (66.67%) (35.19%)
+NYI Anders Lee To record 2+ shots (66.67%) (27.78%)
+OTT Brady Tkachuk To record 4+ shots (66.04%) (30.19%)
+VAN JT Miller To record 2+ shots (65.52%) (27.59%)
+NSH Roman Josi To record 3+ shots (65.45%) (29.09%)
+DAL Tyler Seguin To record 2+ shots (64.29%) (30.36%)
+LAK Arthur Kaliyev To record 2+ shots (64.10%) (30.77%)
+NYI Mike Reilly-2 To record 2+ shots (63.64%) (24.24%)
+LAK Alex Laferriere To record 2+ shots (63.46%) (34.62%)
+PIT Reilly Smith To record 2+ shots (63.04%) (34.78%)
+NSH Cody Glass To record 2+ shots (62.96%) (37.04%)
+COL Ross Colton To record 2+ shots (61.11%) (27.78%)
+NSH Gustav Nyquist To record 2+ shots (60.00%) (30.91%)
+NYR Jacob Trouba To record 2+ shots (59.62%) (40.38%)
+NJD Jesper Bratt To record 3+ shots (58.18%) (27.27%)
+DAL Thomas Harley To record 2+ shots (56.60%) (28.30%)
+VAN Filip Hronek To record 2+ shots (56.14%) (26.32%)
+NYR Alexis Lafreniere To record 3+ shots (53.70%) (31.48%)
+WPG Mark Scheifele To record 3+ shots (53.19%) (38.30%)
+FLA Aleksander Barkov To record 3+ shots (53.06%) (24.49%)
+WPG Gabriel Vilardi To record 3+ shots (51.52%) (24.24%)
+NYR Artemi Panarin To record 4+ shots (50.00%) (22.22%)
+COL Mikko Rantanen To record 4+ shots (50.00%) (26.79%)
+OTT Thomas Chabot To record 3+ shots (50.00%) (26.67%)
+VAN Elias Lindholm To record 3+ shots (50.00%) (24.14%)
+WSH Max Pacioretty To record 3+ shots (50.00%) (27.78%)
+VGK Jonathan Marchessault To record 4+ shots (49.09%) (21.82%)
+WPG Joshua Morrissey To record 3+ shots (49.06%) (26.42%)
+DAL Wyatt Johnston To record 3+ shots (48.21%) (25.00%)
+NJD Jack Hughes To record 5+ shots (47.37%) (21.05%)
+NYR Mika Zibanejad To record 3+ shots (47.17%) (22.64%)
+WPG Nino Niederreiter To record 3+ shots (47.17%) (24.53%)
+NJD Nico Hischier To record 3+ shots (46.51%) (27.91%)
+MIN Ryan Hartman To record 3+ shots (46.00%) (26.00%)
+NSH Filip Forsberg To record 5+ shots (43.86%) (22.81%)
+FLA Carter Verhaeghe To record 4+ shots (43.64%) (25.45%)
+VGK Mark Stone To record 3+ shots (40.00%) (27.27%)
+VGK Ivan Barbashev To record 0+ shots (27.27%) (27.27%)
+NYR Adam Fox To record 0+ shots (25.00%) (25.00%)
+FLA Anton Lundell To record 0+ shots (21.57%) (21.57%)`;
+
+// Store content as an array instead of a string
+const contentArray = contentText.split('\n');
+
+// Function to get border color based on team abbreviation
+function getTeamColor(abbreviation) {
+    const colors = {
+        'ANA': '#B9975B',
+        'ARI': '#8C2633',
+        'BOS': '#FFB81C',
+        'BUF': '#003087',
+        'CGY': '#D2001C',
+        'CAR': '#CE1126',
+        'CHI': '#CF0A2C',
+        'COL': '#6F263D',
+        'CBJ': '#002654',
+        'DAL': '#006847',
+        'DET': '#CE1126',
+        'EDM': '#FF4C00',
+        'FLA': '#C8102E',
+        'LAK': '#111111',
+        'MIN': '#154734',
+        'MTL': '#AF1E2D',
+        'NSH': '#FFB81C',
+        'NJD': '#CE1126',
+        'NYI': '#00539B',
+        'NYR': '#0038A8',
+        'OTT': '#DA1A32',
+        'PHI': '#F74902',
+        'PIT': '#FCB514',
+        'SJS': '#006D75',
+        'SEA': '#99D9D9',
+        'STL': '#002F87',
+        'TBL': '#002868',
+        'TOR': '#00205B',
+        'VAN': '#00205B',
+        'VGK': '#B4975A',
+        'WSH': '#C8102E',
+        'WPG': '#041E42'
+    };
+    return colors[abbreviation] || '#000000'; // Return black if no matching abbreviation found
+}
+
+// Function to set content in the HTML
+function setContent() {
+    const contentDiv = document.getElementById('content');
+    // Iterate over the content array
+    contentArray.forEach(line => {
+        // Remove the first word (team abbreviation) from the line
+        const lineWithoutAbbreviation = line.split(' ').slice(1).join(' ');
+        
+        // Extract relevant data from the line
+        const lineElements = lineWithoutAbbreviation.split(' ');
+        //const firstname = lineElements[0].toLowerCase(); // First name (assumed to be the first word)
+        //const lastname = lineElements[1].toLowerCase(); // Last name (assumed to be the second word)
+        
+        // Construct the player statistics URL
+        //const playerStatsURL = `https://www.foxsports.com/nhl/${firstname}-${lastname}-player-game-log?season=2023&seasonType=reg`;
+
+        const lineContainer = document.createElement('div'); // Create container for each line
+        lineContainer.classList.add('line-container', 'oswald-font'); // Add CSS classes for styling
+        lineContainer.style.display = 'flex'; // Set display to flex
+
+        // Extract team abbreviation from the line and convert it to uppercase
+        const abbreviation = line.split(' ')[0].toUpperCase(); // Assuming the abbreviation is the first word
+
+        lineContainer.style.borderColor = getTeamColor(abbreviation);
+
+        // Create an image element
+        const logoImg = document.createElement('img');
+        logoImg.src = `https://assets.nhle.com/logos/nhl/svg/${abbreviation}_light.svg`; // Set the source URL
+        logoImg.alt = `${abbreviation} Logo`; // Set alt text for accessibility
+        logoImg.style.maxHeight = '1.5em'; // Set maximum height
+        logoImg.style.marginRight = '10px'; // Add right margin to create space between image and text
+        logoImg.style.flexShrink = '0'; // Prevent image from shrinking
+
+        // Append the image before the line
+        lineContainer.appendChild(logoImg);
+
+        // Create a div for the line text
+        const lineDiv = document.createElement('div');
+        lineDiv.textContent = lineWithoutAbbreviation.trim(); // Use the modified line without the abbreviation
+
+        // Append line text to container
+        lineContainer.appendChild(lineDiv);
+
+        // Construct tooltip text using relevant data from the line
+        //const prop = lineElements[2].toString().toLowerCase();
+        //const outof5 = lineElements[5].split('/')[0].slice(1);
+        //const outof10 = lineElements[6].split('/')[0].slice(1);
+        //const streak = lineElements[7].slice(1);
+        //const tooltipText = `Hit the ${prop} in:\n${outof5} of last five games\n${outof10} of last ten games\n${streak} straight games`;
+
+        // Add tooltip
+        //lineContainer.setAttribute('title', tooltipText);
+
+        // Add click event listener to open player statistics URL in a new page
+        //lineContainer.addEventListener('click', function() {
+            //window.open(playerStatsURL, '_blank');
+        //});
+
+        // Append container to content
+       contentDiv.appendChild(lineContainer);
+    });
+}
+
+function getCurrentDate() {
+    const dateNow = new Date();
+    const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+    return dateNow.toLocaleDateString('en-US', dateOptions);
+}
+
+function getCurrentDateTime() {
+    const now = new Date();
+    const options = { hour: 'numeric', minute: 'numeric', hour12: true, timeZoneName: 'short', timeZone: 'America/Chicago' };
+    return now.toLocaleString('en-US', options);
+}
+
+
+window.onload = function() {
+    const currentSubtitle = document.getElementById('subtitle');
+    const currentTime = getCurrentDateTime();
+    currentSubtitle.innerText = `Hover to see details, click to view stats (Updated ${currentTime})`;
+    const currentDateSpan = document.getElementById('date');
+    currentDateSpan.innerText = getCurrentDate();
+    setContent();
+};
+
+// Add this JavaScript to highlight the active link based on the current page
+document.addEventListener('DOMContentLoaded', function() {
+    var currentUrl = window.location.href;
+    var navLinks = document.querySelectorAll('.nav-link');
+
+    navLinks.forEach(function(navLink) {
+        if (navLink.href === currentUrl) {
+            navLink.classList.add('active');
+        }
+    });
+});
+
